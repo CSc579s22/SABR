8e7543c27c8132387ddf14449f2bc4cba9f614a0
abstractionAlpha <63812007+abstractionAlpha@users.noreply.github.com>
2021-08-09 10:36:42 -0400

    Fixed clearing of old directories

--- results_parse/getmultipleruns_BOLA.sh
+++ results_parse/getmultipleruns_BOLA.sh
@@ -8,9 +8,18 @@ CLIENT_IPS=(<<raw client IP address separated by spaces>>)
 counter=0
 for ip in "${CLIENT_IPS[@]}"
 do
-    rm dash_runtime${counter}
-    rm dash_buffer${counter}
-    rm server_log${counter}
+    if [ -d "dash_runtime${counter}" ]
+    then
+        rm -r dash_runtime${counter}
+    fi
+    if [ -d "dash_buffer${counter}" ]
+    then
+        rm -r dash_buffer${counter}
+    fi
+    if [ -d "server_log${counter}" ]
+    then
+        rm -r server_log${counter}
+    fi
     mkdir dash_runtime${counter}
     scp -i ~/.ssh/${KEY_NAME} -P ${PORT_NUMBER} ${USER}@${ip}:/users/${USER}/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime${counter}/
     mkdir dash_buffer${counter}

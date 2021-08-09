e4e61d44ad3a5ce479cacaf55cb4b37783daba81
abstractionAlpha <63812007+abstractionAlpha@users.noreply.github.com>
2021-08-09 10:19:24 -0400

    Made getmultipleruns script remove old folders

--- results_parse/getmultipleruns_BOLA.sh
+++ results_parse/getmultipleruns_BOLA.sh
@@ -3,16 +3,19 @@
 USER="<usr>"
 KEY_NAME="<ssh_key_filename>"  # The path is handled in the loop - only do the name
 PORT_NUMBER=22
-CLIENT_IPS=<<client IP address list>>
+CLIENT_IPS=(<<raw client IP address separated by spaces>>)
 
 counter=0
-for ip in $CLIENT_IPS
+for ip in "${CLIENT_IPS[@]}"
 do
+    rm dash_runtime${counter}
+    rm dash_buffer${counter}
+    rm server_log${counter}
     mkdir dash_runtime${counter}
     scp -i ~/.ssh/${KEY_NAME} -P ${PORT_NUMBER} ${USER}@${ip}:/users/${USER}/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime${counter}/
-    mkdir dash_buffer$(COUNTER)
+    mkdir dash_buffer${counter}
     scp -i ~/.ssh/${KEY_NAME} -P ${PORT_NUMBER} ${USER}@${ip}:/users/${USER}/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer${counter}/
-    mkdir server_log$(COUNTER)
+    mkdir server_log${counter}
     scp -i ~/.ssh/${KEY_NAME} -P ${PORT_NUMBER} ${USER}@${ip}:/users/${USER}/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log${counter}/
     counter=$((counter+1))
 done

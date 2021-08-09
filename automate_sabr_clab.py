a9fed2ffc0caae111215d889332b536d1f47e85b
abstractionAlpha <63812007+abstractionAlpha@users.noreply.github.com>
2021-08-09 09:06:18 -0400

    Put ssh.exec_command before calls to sterr

--- automate_sabr_clab.py
+++ automate_sabr_clab.py
@@ -89,8 +89,8 @@ def dash_client(ipaddress, ports, zipf_index, mpd_ip):
     print('[+] '+ works)
     #Insert relevant player command here
     cl_command = "cd /users/dbhat0/astream_dash_bolao; python dist/client/dash_client.py -m http://"+str(mpd_ip)+"/BigBuckBunny_2s_mod" + str(int(zipf_index)+1) + "/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/BigBuckBunny_2s_mod" +str(int(zipf_index)+1)+ ".mpd -p bola > /dev/null &"
-    stringerr = str(stderr.readlines()[1:]) + str(ipaddress) + str(ports)
     stdin,stdout,stderr=ssh.exec_command(cl_command)
+    stringerr = str(stderr.readlines()[1:]) + str(ipaddress) + str(ports)
     stringout = str(stderr.readlines()) + str(ipaddress) + str(ports)
     print(stringout)
     ssh.close()

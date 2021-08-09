748aa63038f7677a96e6085a02c1976bf70c9f04
abstractionAlpha <63812007+abstractionAlpha@users.noreply.github.com>
2021-08-09 09:59:49 -0400

    Changed BASH syntax to be appropriate(?)

--- results_parse/getmultipleruns_BOLA.sh
+++ results_parse/getmultipleruns_BOLA.sh
@@ -8,11 +8,11 @@ CLIENT_IPS=<<client IP address list>>
 counter=0
 for ip in $CLIENT_IPS
 do
-    mkdir dash_runtime$(counter)
-    scp -i ~/.ssh/$(KEY_NAME) -P $(PORT_NUMBER) $(USER)@$(ip):/users/$(USER)/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime$(COUNTER)/
+    mkdir dash_runtime${counter}
+    scp -i ~/.ssh/${KEY_NAME} -P ${PORT_NUMBER} ${USER}@${ip}:/users/${USER}/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime${counter}/
     mkdir dash_buffer$(COUNTER)
-    scp -i ~/.ssh/$(KEY_NAME) -P $(PORT_NUMBER) $(USER)@$(ip):/users/$(USER)/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer$(COUNTER)/
+    scp -i ~/.ssh/${KEY_NAME} -P ${PORT_NUMBER} ${USER}@${ip}:/users/${USER}/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer${counter}/
     mkdir server_log$(COUNTER)
-    scp -i ~/.ssh/$(KEY_NAME) -P $(PORT_NUMBER) $(USER)@$(ip):/users/$(USER)/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log$(COUNTER)/
+    scp -i ~/.ssh/${KEY_NAME} -P ${PORT_NUMBER} ${USER}@${ip}:/users/${USER}/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log${counter}/
     counter=$((counter+1))
 done

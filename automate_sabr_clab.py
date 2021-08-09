69e843fd74543c9f011638fa6603668120c47563
abstractionAlpha <63812007+abstractionAlpha@users.noreply.github.com>
2021-08-09 10:57:50 -0400

    Updated client command to not be hard-coded

--- automate_sabr_clab.py
+++ automate_sabr_clab.py
@@ -89,7 +89,7 @@ def dash_client(ipaddress, ports, zipf_index, mpd_ip):
     works = ipaddress.strip('\n')+','+user  
     print('[+ client] '+ works)
     #Insert relevant player command here
-    cl_command = "cd /users/dbhat0/astream_dash_bolao; python dist/client/dash_client.py -m http://"+str(mpd_ip)+"/BigBuckBunny_2s_mod" + str(int(zipf_index)+1) + "/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/BigBuckBunny_2s_mod" +str(int(zipf_index)+1)+ ".mpd -p bola > /dev/null &"
+    cl_command = "cd /home/" + user + "/astream_dash_bolao; python dist/client/dash_client.py -m http://"+str(mpd_ip)+"/BigBuckBunny_2s_mod" + str(int(zipf_index)+1) + "/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/BigBuckBunny_2s_mod" +str(int(zipf_index)+1)+ ".mpd -p bola > /dev/null &"
     try:
         stdin,stdout,stderr=ssh.exec_command(cl_command)
         stringout = stdout.read().decode('ascii').strip("\n")
